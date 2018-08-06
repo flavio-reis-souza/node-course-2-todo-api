8d290fef1055df4ac20fa26c1cf47913b3ee754f
bob <flavio.bnu@gmail.com>
2018-08-06 10:49:04 -0300

    Add POST /todos rout and rtefactor mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,23 @@
+var mongoose = require('mongoose');
+
+var Todo = mongoose.model('Todo', {
+  text: {
+    type: String,
+    required: true,
+    minlength: 1,
+    trim:true
+  },
+  completed:{
+    type: Boolean,
+    default:false
+  },
+  completedAt:{
+    type: Number,
+    default: null
+  },
+  location:{
+    type: String
+  }
+});
+
+module.exports = {Todo};
