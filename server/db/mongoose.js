7aa6a277cd4f21e62f778cfc1b39b91d4d01efda
bob <flavio.bnu@gmail.com>
2018-08-31 07:07:22 -0300

    Add tests FOR PATCH /todos/:id

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promisse = global.Promisse;
+// mongoose.connect('mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 mongoose.connect('mongodb://curso-node:!Mari8746@ds157509.mlab.com:57509/todos' || 'mongodb://localhost:27017/TodoApp', { useNewUrlParser: true });
 
 module.exports = {mongoose};
